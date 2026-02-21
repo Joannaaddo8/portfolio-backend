70afe69092770173c179aa6ba737482f3d2e001f
Joannaaddo8 <jaddo8@my.centennialcollege.ca>
2026-02-21 11:30:47 -0500

    Updated PORT for deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import referenceRoutes from './routes/reference.routes.js';
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connect Database
 dotenv.config();
